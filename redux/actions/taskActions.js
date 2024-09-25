7229ffd9b90519fcc47a6ac2c5fd964bc126ed59
hasansisik <gegify@gmail.com>
2024-09-25 21:36:16 +0300

    Add sendNotification helper function for push notifications

--- redux/actions/taskActions.js
+++ redux/actions/taskActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { server } from "../../config";
+import sendNotification from "../../helpers/sendNotification";
 
 export const createTask = createAsyncThunk(
   "task/create",
